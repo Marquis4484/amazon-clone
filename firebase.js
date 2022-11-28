c050c53f50a7ca56d6248252bb1f14f871830ab8
Marquis Sampson <marquis4484@gmail.com>
2022-11-28 11:25:27 -0500

    redux portion complete

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+   apiKey: "AIzaSyBiicoLmn9A2BfSt-hDc_qJYEbHTXqhW9A",
+   authDomain: "amzn-clone-2de39.firebaseapp.com",
+   projectId: "amzn-clone-2de39",
+   storageBucket: "amzn-clone-2de39.appspot.com",
+   messagingSenderId: "883434137282",
+   appId: "1:883434137282:web:1c1788443504169324fd5b",
+};
