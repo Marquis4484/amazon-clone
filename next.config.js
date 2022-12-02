886926a690893d690260bbd57b0b8d4dfc22ad47
Marquis Sampson <marquis4484@gmail.com>
2022-12-01 22:53:55 -0500

    stripe added

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
    images: {
       domains: ["links.papareact.com", "fakestoreapi.com"], // whitelisting images for Image Tag
    },
+
+   env: {
+      stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
+   },
 };
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -6,11 +6,30 @@ import { selectItems, selectTotal } from "../slices/basketSlice";
 import CheckoutProduct from "../components/CheckoutProduct";
 import Currency from "react-currency-formatter";
 import { useSession } from "next-auth/react";
+import { loadStripe } from "@stripe/stripe-js";
+const stripePromise = loadStripe(process.env.stripe_public_key);
+import axios from "axios";
 
 function Checkout() {
    const items = useSelector(selectItems);
    const { data: session } = useSession();
    const total = useSelector(selectTotal);
+
+   const createCheckoutSession = async () => {
+      const stripe = await stripePromise;
+
+      // Calling backend to create checkout session
+
+      const checkoutSession = await axios.post("/api/create-checkout-session", {
+         items: items,
+         email: session.user.email,
+      });
+
+      const result = await stripe.redirectToCheckout({
+         sessionId: checkoutSession.data.id,
+      });
+      if (result.error) alert(result.error.message);
+   };
    return (
       <div className="bg-gray-100">
          <Header />
@@ -57,6 +76,9 @@ function Checkout() {
                         </span>
                      </h2>
                      <button
+                        onClick={createCheckoutSession}
+                        role="link"
+                        disabled={!session}
                         className={`button mt-2 ${
                            !session &&
                            "from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed"
