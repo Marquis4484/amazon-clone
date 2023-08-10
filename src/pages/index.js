febed837fa26bce457d19aac17fad9e0e6185f18
Marquis Sampson <marquis4484@gmail.com>
2023-08-10 15:10:14 -0400

    Making Changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+//index.js is HomePage
 import Head from "next/head";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
@@ -5,31 +6,31 @@ import ProductFeed from "../components/ProductFeed";
 import { getSession } from "next-auth/react";
 
 export default function Home({ products }) {
-   return (
-      <div className="bg-gray-100">
-         <Head>
-            <title>Amazon 2.0</title>
-         </Head>
+  return (
+    <div className="bg-gray-100">
+      <Head>
+        <title>Amazon Clone</title>
+      </Head>
 
-         <Header />
-         <main className="max-w-screen-2xl mx-auto">
-            <Banner />
-            <ProductFeed products={products} />
-         </main>
-      </div>
-   );
+      <Header />
+      <main className="max-w-screen-2xl mx-auto">
+        <Banner />
+        <ProductFeed products={products} />
+      </main>
+    </div>
+  );
 }
 
 //server side rendering to retrieve data from Fakeapi store
 export async function getServerSideProps(context) {
-   const session = await getSession(context);
-   const products = await fetch("https://fakestoreapi.com/products").then(
-      (res) => res.json()
-   );
-   return {
-      props: {
-         products, // shorthand version ES6 from products: products
-         session,
-      },
-   };
+  const session = await getSession(context);
+  const products = await fetch("https://fakestoreapi.com/products").then(
+    (res) => res.json()
+  );
+  return {
+    props: {
+      products, // shorthand version ES6 from products: products
+      session,
+    },
+  };
 }
