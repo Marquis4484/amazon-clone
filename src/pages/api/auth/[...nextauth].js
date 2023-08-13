75820065ba045bb9dfd3dd28ceedeba7e0ca9b25
Marquis Sampson <marquis4484@gmail.com>
2023-08-13 13:13:23 -0400

    Comments added

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -3,19 +3,19 @@ import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
 
 export const authOptions = {
-   // Configure one or more authentication providers
-   providers: [
-      GoogleProvider({
-         clientId: process.env.GOOGLE_CLIENT_ID,
-         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      }),
-      GitHubProvider({
-         clientId: process.env.GITHUB_ID,
-         clientSecret: process.env.GITHUB_SECRET,
-      }),
-      // ...add more providers here
-   ],
-   secret: process.env.NEXTAUTH_SECRET, // secret for production
+  // Github and google providers added
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    // can add more providers here
+  ],
+  secret: process.env.NEXTAUTH_SECRET, // secret for production
 };
 
 export default NextAuth(authOptions);
