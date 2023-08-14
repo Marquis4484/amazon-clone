c7c5b1e1504366bc218e4bc1e750704d5ae616dd
Marquis Sampson <marquis4484@gmail.com>
2023-08-14 15:41:22 -0400

    Modified Firebase File

--- firebase.js
+++ firebase.js
@@ -1,17 +1,17 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-   apiKey: "AIzaSyBiicoLmn9A2BfSt-hDc_qJYEbHTXqhW9A",
-   authDomain: "amzn-clone-2de39.firebaseapp.com",
-   projectId: "amzn-clone-2de39",
-   storageBucket: "amzn-clone-2de39.appspot.com",
-   messagingSenderId: "883434137282",
-   appId: "1:883434137282:web:1c1788443504169324fd5b",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 const app = !firebase.apps.length
-   ? firebase.initializeApp(firebaseConfig)
-   : firebase.app();
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
 
 const db = app.firestore();
 
