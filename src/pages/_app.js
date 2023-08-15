e2da4f80a4fd2e58bb193196fbd2a22c0c6ba981
Marquis Sampson <marquis4484@gmail.com>
2023-08-15 13:03:38 -0400

    Changes added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,16 @@
 import { Provider } from "react-redux";
 import { store } from "../app/store";
 import "../styles/globals.css";
-import { SessionProvider } from "next-auth/react"; //Auth State passed throughout app
+import { SessionProvider } from "next-auth/react"; //Auth Signed-in State passed throughout app
 
 const MyApp = ({ Component, pageProps }) => {
-   return (
-      <SessionProvider session={pageProps.session}>
-         <Provider store={store}>
-            <Component {...pageProps} />
-         </Provider>
-      </SessionProvider>
-   );
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </SessionProvider>
+  );
 };
 
 export default MyApp;
